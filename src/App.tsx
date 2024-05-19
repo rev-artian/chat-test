59069cb8956b6a04b9bcd8dfcdd675f378612c3f
Rhyza <rhyza@artian.ai>
2024-05-19 16:12:33 -0400

    Handle ai response

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,52 @@
 import { useCallback, useState } from 'react';
-import OpenAI from 'openai';
+import { openai } from './openai';
 
 import Conversation from './components/Conversation';
 import MessageInput from './components/MessageInput';
 
+type Message = {
+  body: string;
+  sender: string;
+};
+
 export default function App() {
-  const [conversations, setConversations] = useState('');
+  const [messages, setMessages] = useState(new Array<Message>());
   const [aiMessage, setAiMessage] = useState('');
   const [userMessage, setUserMessage] = useState('');
 
-  const handleSubmit = useCallback(async (e) => {
-    //
-  }, []);
+  const handleSubmit = useCallback(async (e: any) => {
+    e.preventDefault();
+
+    setMessages((prev) => [
+      ...prev,
+      {
+        body: userMessage,
+        sender: 'human',
+      },
+    ]);
+    setUserMessage(() => '');
+
+    const stream = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: userMessage }],
+      stream: true,
+    });
+
+    for await (const part of stream) {
+      setAiMessage((prev) => prev + part.choices[0].delta.content);
+
+      if (part.choices[0].finish_reason === 'stop') {
+        setMessages((prev) => [...prev, { body: aiMessage, sender: 'ai' }]);
+        setAiMessage(() => '');
+        break;
+      }
+    }
+  }, [userMessage]);
 
   return (
     <div className="flex w-full justify-center">
       <div className="flex h-screen w-full max-w-[800px] flex-col justify-between p-6">
-        <Conversation />
+        <Conversation messages={messages} />
         <MessageInput
           userMessage={userMessage}
           setUserMessage={setUserMessage}
